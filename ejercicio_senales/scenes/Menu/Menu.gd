c44784756e937c083164e4c11faa944d6c2b43b9
Zek2402 <eze.erbes.92@gmail.com>
2025-04-28 23:17:40 -0300

    6- crear contador

--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -5,9 +5,13 @@ const INSTANCES_TEXT = "Cantidad de instancias: "
 var instancesCount = 0
 
 
-
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	pass
+
+func _count_new_instance():
+	instancesCount += 1
+	$CountLabel.text = str(instancesCount)
+	
